da11f01a65df4b157e86ae4a9e8ab73e771ae0d9
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-15 21:42:20 +0100

    feat(Gold) : record Gold (Or)

---
+++ BackTrack/src/fr/uge/model/Gold.java
@@ -0,0 +1,13 @@
+package fr.uge.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public record Gold(int number,List<Coordonate> coordonnates, Rarity rarity) {
+	public Gold {
+		Objects.requireNonNull(rarity);
+		if(number < 0) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
