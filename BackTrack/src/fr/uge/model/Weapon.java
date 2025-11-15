215e8e33b081e34da982c130ede60630855230ee
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-15 21:13:33 +0100

    feat(Weapon) : un record Weapon pour les armes

---
+++ BackTrack/src/fr/uge/model/Weapon.java
@@ -0,0 +1,22 @@
+package fr.uge.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public record Weapon(String name, WeaponType type, WeaponClass classe, 
+		int cost, int healthPoint, List<Coordonate> coordonates, Rarity rarity) implements Item{
+	public Weapon {
+		Objects.requireNonNull(name);
+		Objects.requireNonNull(type);
+		Objects.requireNonNull(classe);
+		Objects.requireNonNull(rarity);
+		if( cost < 0) {
+			throw new IllegalArgumentException("Cost n'accepte pas une valeur négative");
+		}
+		if(healthPoint < 0) {
+			throw new IllegalArgumentException(" Point de vie négatif inacceptable");
+		}
+	}
+	
+	
+}
