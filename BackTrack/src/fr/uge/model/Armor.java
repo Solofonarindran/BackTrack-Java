a12e16bbb4db4ed69d16a861a4f91c10c3ae87c2
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-15 20:33:04 +0100

    feat(Armor) : record Armor pour les matériels de défense

---
+++ BackTrack/src/fr/uge/model/Armor.java
@@ -0,0 +1,19 @@
+package fr.uge.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public record Armor(String name, ArmorType type, Rarity rarity, List<Coordonate> coordonates, 
+	int cost, int defensePoint) implements Item{
+	public Armor {
+		Objects.requireNonNull(name);
+		Objects.requireNonNull(type);
+		Objects.requireNonNull(rarity);
+		if(cost < 0) {
+			throw new IllegalArgumentException("cost valeur négatif inacceptable");
+		}
+		if(defensePoint < 0) {
+			throw new IllegalArgumentException("Point de défense négatif inacceptable");
+		}
+	}
+}
