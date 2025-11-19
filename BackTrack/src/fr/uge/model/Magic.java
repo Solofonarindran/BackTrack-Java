444c5cef8fdd240252bcc7a855d93022480ea5c3
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-19 18:08:02 +0100

    chore( Magic) : add urlmage and change coordonates to references

--- BackTrack/src/fr/uge/model/Magic.java
+++ BackTrack/src/fr/uge/model/Magic.java
@@ -3,10 +3,11 @@ package fr.uge.model;
 import java.util.List;
 import java.util.Objects;
 
-public record Magic(String name, MagicType type, Rarity rarity, List<Coordonate> coordonates, int manaMax) implements Item, ItemTreasure{
+public record Magic(String name, MagicType type, Rarity rarity, int manaMax, String urlImage, List<Coordonate> references) implements Item, ItemTreasure{
 	public Magic {
 		Objects.requireNonNull(name);
 		Objects.requireNonNull(type);
+		Objects.requireNonNull(urlImage);
 		if (manaMax < 0) {
 			throw new IllegalArgumentException();
 		}
