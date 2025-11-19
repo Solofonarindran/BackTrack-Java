b6c580466ba87789747c8fac070db084556d1b40
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-19 18:13:01 +0100

    feat(Treasure) : treasure record

---
+++ BackTrack/src/fr/uge/data/Treasure.java
@@ -0,0 +1,16 @@
+package fr.uge.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.uge.model.ItemTreasure;
+
+public class Treasure {
+	private final ArrayList<ItemTreasure> treasures = new ArrayList<ItemTreasure>();
+	
+	public static List<ItemTreasure> tresor() {
+		return List.of();
+		
+	}
+	
+}
