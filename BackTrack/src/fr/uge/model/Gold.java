75d0f777d321f3a72a3da29e8c2fb285292e6ecc
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-19 18:04:02 +0100

    chore(Gold) : change coordonates to references

--- BackTrack/src/fr/uge/model/Gold.java
+++ BackTrack/src/fr/uge/model/Gold.java
@@ -3,9 +3,10 @@ package fr.uge.model;
 import java.util.List;
 import java.util.Objects;
 
-public record Gold(int number,List<Coordonate> coordonnates, Rarity rarity) implements Item, ItemTreasure{
+public record Gold(int number, Rarity rarity, String urlImage, List<Coordonate> references) implements Item, ItemTreasure{
 	public Gold {
 		Objects.requireNonNull(rarity);
+		Objects.requireNonNull(urlImage);
 		if(number < 0) {
 			throw new IllegalArgumentException();
 		}
