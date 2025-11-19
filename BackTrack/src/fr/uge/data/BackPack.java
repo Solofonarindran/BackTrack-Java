3eef92177f2b8e9de5a67dac9bf9c81aa28c4380
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-19 17:46:34 +0100

    chore(BlackTrack) : change List<Item> to Map<Item, List<Coordonate>>

--- BackTrack/src/fr/uge/data/BackPack.java
+++ BackTrack/src/fr/uge/data/BackPack.java
@@ -1,20 +1,21 @@
 package fr.uge.data;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import fr.uge.model.Coordonate;
 import fr.uge.model.Item;
 
 public class BackPack {
-	private final ArrayList<Item> items ;
+	private final HashMap<Item, List<Coordonate>> items ;
 	
 	// ce map contient les places (coordonnées) qui sont déverouillés (unlock) et dispo
 	private final HashMap<Coordonate,Map<String,Boolean>> coordonates;
 	
 	public BackPack() {
-		items = new ArrayList<Item>();
+		items = new HashMap<Item, List<Coordonate>>();
 		coordonates = new HashMap<Coordonate,Map<String,Boolean>>();
 	}
 	
