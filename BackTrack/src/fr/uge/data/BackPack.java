e08a6685619fd75089efce86c60f8f6e04321606
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-20 16:36:48 +0100

    feat(BackTrack) : add method private to check if the coordonate is valid

--- BackTrack/src/fr/uge/data/BackPack.java
+++ BackTrack/src/fr/uge/data/BackPack.java
@@ -4,19 +4,37 @@ package fr.uge.data;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import fr.uge.model.Coordonate;
 import fr.uge.model.Item;
 
 public class BackPack {
-	private final HashMap<Item, List<Coordonate>> items ;
+	private final HashMap<Item, List<Coordonate>> equipements ;
 	
 	// ce map contient les places (coordonnées) qui sont déverouillés (unlock) et dispo
+	
 	private final HashMap<Coordonate,Map<String,Boolean>> coordonates;
 	
 	public BackPack() {
-		items = new HashMap<Item, List<Coordonate>>();
+		equipements = new HashMap<Item, List<Coordonate>>();
 		coordonates = new HashMap<Coordonate,Map<String,Boolean>>();
+	}
+	
+	
+	private boolean isAccepted(Item equipement, Coordonate coordonate) {
+		
+		Objects.requireNonNull(equipement);
+		Objects.requireNonNull(coordonate);
+		return coordonates.entrySet().stream()
+																 .filter(e->e.getKey().equals(coordonate) && e.getValue().get("unlocked"))
+																 .map(e->e.getValue().get("dispo"))
+																 .findFirst()
+																 .orElseGet(()->false);
+	}
+	
+	public void addEquipement(Item equipement) {
+		Objects.requireNonNull(equipement);
 	}
 	
 }
