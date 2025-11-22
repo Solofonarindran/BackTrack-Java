6a3e979d572e61df0e95b799fd27c0b8edb4c884
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-22 15:07:33 +0100

    feat(BlackPack) : create addEquipement to add equipement in BackPack

--- BackTrack/src/fr/uge/data/BackPack.java
+++ BackTrack/src/fr/uge/data/BackPack.java
@@ -1,6 +1,7 @@
 package fr.uge.data;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,14 +11,14 @@ import fr.uge.model.Coordonate;
 import fr.uge.model.Item;
 
 public class BackPack {
-	private final HashMap<Item, List<Coordonate>> equipements ;
+	private final HashMap<Item, List<Coordonate>> equipments ;
 	
 	// ce map contient les places (coordonnées) qui sont déverouillés (unlock) et dispo
 	
 	private final HashMap<Coordonate,Map<String,Boolean>> coordonates;
 	
 	public BackPack() {
-		equipements = new HashMap<Item, List<Coordonate>>();
+		equipments = new HashMap<Item, List<Coordonate>>();
 		coordonates = new HashMap<Coordonate,Map<String,Boolean>>();
 	}
 	
@@ -35,12 +36,27 @@ public class BackPack {
 		return coordonates.entrySet().stream()
 									 .filter(e->e.getKey().equals(coordonate) && e.getValue().get("unlocked"))
 									 .map(e->e.getValue().get("dispo"))
-								     .findFirst()
+								   .findFirst()
 									 .orElseGet(()->false);
 	}
 	
-	public void addEquipement(Item equipement) {
+	public void addEquipement(Item equipement,Coordonate coordonate) {
+		// coordonate ( données ou coordonnées récuperées venant d'interface zen)
 		Objects.requireNonNull(equipement);
+		Objects.requireNonNull(coordonate);	
+		var references = equipement.references();
+		var coordonateAbsolute = Coordonate.toAbsolute(references, coordonate);
+		// crée une copie pour manipuler 
+		var coordonateAbsoluteCopy = new ArrayList<Coordonate>(coordonateAbsolute);
+		var isAccepted = coordonateAbsoluteCopy.stream()
+													.map(c->isAccepted(equipement, c))
+													.noneMatch(b->!b); // vérifie si aucune réponse est false
+		
+		if(isAccepted) {
+			equipments.put(equipement, coordonateAbsolute);
+		}
+		
+													
 	}
 	
 }
