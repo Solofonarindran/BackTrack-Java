dc3d2a869e8d15251bbd4de4e5492137181c2286
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-26 00:56:25 +0100

    feat(ItemRotation) : add public method rotateItem and private method patternMatching

--- BackTrack/src/fr/uge/data/ItemRotation.java
+++ BackTrack/src/fr/uge/data/ItemRotation.java
@@ -2,10 +2,17 @@ package fr.uge.data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import fr.uge.model.Armor;
+import fr.uge.model.Clef;
+import fr.uge.model.Consumable;
 import fr.uge.model.Coordonate;
+import fr.uge.model.Gold;
 import fr.uge.model.Item;
+import fr.uge.model.Magic;
+import fr.uge.model.Weapon;
 
 public class ItemRotation {
 	
@@ -28,8 +35,33 @@ public class ItemRotation {
 			return normalised;
 	}
 	
-	private boolean canRotate(Item item ) {
+	private static boolean canRotate(Item item ) {
 		Objects.requireNonNull(item);
 		return item.rotate();
+	}
+	
+	private static Map<Item, List<Coordonate>> patternMatching(Item item, List<Coordonate> coordinates, List<Coordonate> newCoordinates) {
+		Objects.requireNonNull(item);
+		return switch (item) {
+			case Armor a -> Map.of(new Armor(a.name(), a.type(), a.rarity(),a.references(), a.cost(),a.defensePoint(),a.urlImage()), newCoordinates);
+			case Weapon w -> Map.of(new Weapon(w.name(), w.type(), w.classe(), w.cost(), w.healthPoint(), w.rarity(), w.references()), newCoordinates);
+			case Magic m -> Map.of(new Magic(m.name(), m.type(), m.rarity(), m.manaMax(), m.urlImage(), m.references()), newCoordinates);
+			case Gold g -> Map.of(new Gold(g.number(), g.rarity(), g.urlImage(),g.references()), newCoordinates);
+			case Consumable c -> Map.of(new Consumable(c.name(), c.references()), newCoordinates);
+			case Clef cf -> Map.of(new Clef(cf.rarity(), cf.urlImage(),cf.references()), newCoordinates);
+			default ->  Map.of(item, coordinates);
+		};
+	}
+	
+	public static Map<Item,List<Coordonate>> rotateItem(Item item, List<Coordonate> coordinates) {
+		Objects.requireNonNull(item);
+		
+		//bloquer Malediction
+		if(!canRotate(item)) {
+			return Map.of(item, coordinates);
+		}
+		var newCoordinates = rotate90Clockwise(coordinates);
+    return patternMatching(item, coordinates, newCoordinates);
+		
 	}
 }
