3a5d0a4ff75a6f68ef6086716fcc0eab4d2f7905
narykely <solofonarindraherinantenaina@gmail.com>
2025-11-28 18:30:03 +0100

    feat(Malediction) : add rotate method to return false

--- BackTrack/src/fr/uge/model/Malediction.java
+++ BackTrack/src/fr/uge/model/Malediction.java
@@ -3,7 +3,12 @@ package fr.uge.model;
 import java.util.List;
 
 public record Malediction(List<Coordonate> size,List<Coordonate> references) implements Item{
-
+	
+	public Malediction {
+		if( references.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+	}
 	@Override
 	public boolean rotate() {
 		return false;
