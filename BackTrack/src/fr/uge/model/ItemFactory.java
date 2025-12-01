4ae86c0af8f1bc82a00379f6778872e4349fa683
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-01 10:40:37 +0100

    feat(ItemFactory): ajout de la classe ItemFactory qui permet d'avoir un catalogue des objets disponibles

---
+++ BackTrack/src/fr/uge/model/ItemFactory.java
@@ -0,0 +1,37 @@
+package fr.uge.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ItemFactory {
+
+    private static List<ItemTreasure> catalog;
+    
+    public ItemFactory() {
+    	catalog = initCatalog();
+    }
+
+    // Méthode qui initialise le catalogue des objets disponibles 
+    private static List<ItemTreasure> initCatalog() {
+        var list = new ArrayList<>();
+        list.addAll(Weapon.weaponsAvailable());
+        list.addAll(Armor.armorAvailable());
+        list.addAll(Magic.magicElemAvailable());
+        list.add(Gold.initGold());
+        // a voir pour les types style <E>
+        return List.copyOf(list);
+    }
+    
+    // utile ?
+    public static List<ItemTreasure> itemsAvailable() {
+        return catalog;
+    }
+    
+    // Méthode qui permet d'obtenir un objet en le recherchant avec son nom -> utile ?
+    public static ItemTreasure getItemByName(String name) {
+        return catalog.stream()
+                      .filter(i -> i.name().equals(name))
+                      .findFirst()
+                      .orElseThrow(() -> new IllegalArgumentException("Item inconnu : " + name));
+    }
+}
