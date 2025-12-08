f8245418db801f3d43dea8e943f8d5ad4260252d
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-08 10:19:54 +0100

    feat(Enemy): passage des champs en private au lieu de private final + ajout du String name + ajout des méthodes d'accès aux paramètres

--- BackTrack/src/fr/uge/data/Enemy.java
+++ BackTrack/src/fr/uge/data/Enemy.java
@@ -1,15 +1,25 @@
 package fr.uge.data;
 
-public class Enemy {
+import java.util.Objects;
+
+public final class Enemy {
 
 	// statistiques de base
-	public int healthPoints; //Points de vie actuels
-	private final int maxHealthPoints; // Points de vie maximum
-	private final int protection; // Point de protection temporaire
+	private int healthPoints; //Points de vie actuels
+	private int maxHealthPoints; // Points de vie maximum
+	private int protection; // Point de protection temporaire
+	private String name;
 	
 	
-	public Enemy(int healthPoints,int maxHealthPoints, int protection) {
-		
+	public Enemy(String name, int healthPoints, int maxHealthPoints, int protection) {
+		Objects.requireNonNull(name);
+		if (maxHealthPoints <= 0) {
+            throw new IllegalArgumentException("Les PV maximum doivent être > 0");
+        }
+        if (protection < 0) {
+            throw new IllegalArgumentException("La protection ne peut être négative");
+        }
+        this.name = name;
 		this.healthPoints = healthPoints;
 		this.maxHealthPoints = maxHealthPoints;
 		this.protection = protection;
@@ -28,32 +38,45 @@ public class Enemy {
 		if(protection > 0) {
 			if(protection >= damage) {
 				newProtection = protection - damage;
-				return new Enemy(healthPoints, maxHealthPoints, newProtection);
-			}else {
+				return new Enemy(name, healthPoints, maxHealthPoints, newProtection);
+			} else {
 				restDamage = damage - protection;
+				newProtection = 0;
 			}
 		}
 	  // si non le dégât s'implique directement au point de vie (healthPoint) de 
 		var newHealthPoints = Math.max(0, healthPoints - restDamage);
-		return new Enemy(newHealthPoints, maxHealthPoints, newProtection);
+		return new Enemy(name, newHealthPoints, maxHealthPoints, newProtection);
 	}
 
 	
 	// méthode pour incrémenter la protection 
 	public Enemy addProtection(int amount) { // On changera le paramètre amount par un item pour garder l'encapsulation
 		var newProtection = protection + amount;
-		return new Enemy(healthPoints, maxHealthPoints, newProtection);
+		return new Enemy(name, healthPoints, maxHealthPoints, newProtection);
 	}
 	
 	// réinitialisé la protection
 	public Enemy resetProtection() {
-		return new Enemy(healthPoints, maxHealthPoints, 0);
+		return new Enemy(name, healthPoints, maxHealthPoints, 0);
 	}
 	
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getProtection() {
+		return protection;
+	}
 	
 	@Override
 	public String toString() {
 		var str = "\n========== STATISTIQUES DE L'ENNEMI ==========" +
+							"    Nom        : " + name +
 							"❤️  Vie        : " + healthPoints + "/" + maxHealthPoints +
 							"🛡️  Protection : " + protection +
 							"==========================================\n";
