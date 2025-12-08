02dd9e280ea3b35a9ad69fbc8df8af30125cbe44
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-08 10:39:45 +0100

    chore(Malediction): ajout des champs name et rarity

--- BackTrack/src/fr/uge/model/Malediction.java
+++ BackTrack/src/fr/uge/model/Malediction.java
@@ -2,7 +2,7 @@ package fr.uge.model;
 
 import java.util.List;
 
-public record Malediction(List<Coordonate> size,List<Coordonate> references) implements Item{
+public record Malediction(String name, Rarity rarity, List<Coordonate> size,List<Coordonate> references) implements Item{
 	
 	public Malediction {
 		if( references.isEmpty()) {
