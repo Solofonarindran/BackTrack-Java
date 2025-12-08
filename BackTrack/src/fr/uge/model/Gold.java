321bd0fac9c7d3f4e189fb6f86aacbbac27bb350
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-08 10:26:26 +0100

    feat(Gold): ajout int quantity + ajout des méthodes de gestion de quantité + remplacement de l'implentation d'ItemTreasure par Ressources, on a séparé les item de combat et les type de monnaie

--- BackTrack/src/fr/uge/model/Gold.java
+++ BackTrack/src/fr/uge/model/Gold.java
@@ -2,21 +2,47 @@ package fr.uge.model;
 
 import java.util.List;
 import java.util.Objects;
-
-public record Gold(String name, int number, int damage, int protection, Rarity rarity, String urlImage, List<Coordonate> references) implements Item, ItemTreasure{
+//String urlImage
+public record Gold(String name, Rarity rarity, int quantity,  List<Coordonate> references) implements Item, Ressources {
+	
 	public Gold {
 		Objects.requireNonNull(rarity);
-		Objects.requireNonNull(urlImage);
-		if(number < 0) {
+		//Objects.requireNonNull(urlImage);
+		if(quantity < 0) {
 			throw new IllegalArgumentException();
 		}
 	}
 	
 	public static Gold initGold() {
 		 List<Coordonate> refs = List.of(new Coordonate(0, 0));
-		 var urlGold = "D:/L3/Java/images/Gold.png";
-	     var gold = new Gold("Gold", 1, 0, 0, Rarity.COMMON, urlGold, refs);
+		 //var urlGold = "D:/L3/Java/images/Gold.png";
+	     var gold = new Gold("Gold", Rarity.COMMON, 0, refs);
 	     return gold;
 	}
+	
+	 
+	// créé un nouveau Gold si par exemple on a gagné un "tas"
+	public static Gold of(int quantity) {
+	    return new Gold("Gold", Rarity.COMMON, quantity, List.of(new Coordonate(0,0)));
+	}
 
+	
+	public Gold spend(int amount) {
+	    if (amount < 0) {
+	        throw new IllegalArgumentException("Montant négatif impossible");
+	    }
+	    if (quantity < amount) {
+	        throw new IllegalStateException("Pas assez d'or !");
+	    }
+
+	    return new Gold(name, rarity, quantity- amount, references);
+	}
+	
+	public Gold add(int amount) {
+	    if (amount < 0) {
+	        throw new IllegalArgumentException("Montant négatif impossible");
+	    }
+
+	    return new Gold(name, rarity, quantity + amount, references);
+	}
 }
