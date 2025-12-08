8183293a09b5721fe97bc6fb2fa20793b892a34e
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-08 10:24:49 +0100

    feat(FightItemsFactory): création de la classe FightItemsFactory qui permet d'avoir la liste des objets de combat à partir de la liste des items dans le sac

---
+++ BackTrack/src/fr/uge/model/FightItemsFactory.java
@@ -0,0 +1,24 @@
+package fr.uge.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FightItemsFactory {
+
+	private static final List<FightItem> FIGHT_ITEMS = initFightItems();
+		
+	  // Méthode qui initialise le catalogue des objets de combat disponibles 
+	public static List<FightItem> initFightItems() {
+		List<FightItem> list = new ArrayList<>();
+		for(var item: ItemFactory.getCatalog()) {
+			if (item instanceof FightItem fightItem) {
+				list.add(fightItem);
+			}
+		}
+	    return List.copyOf(list);    
+	}
+	  
+	public static List<FightItem> getfightItems() {
+	    return FIGHT_ITEMS;
+     }
+}
