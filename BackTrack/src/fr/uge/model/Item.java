435f815f3a05f74555a360d535f7314d14017a8e
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-08 10:30:01 +0100

    chore(Item): ajout des champs name et rarity

--- BackTrack/src/fr/uge/model/Item.java
+++ BackTrack/src/fr/uge/model/Item.java
@@ -3,6 +3,8 @@ package fr.uge.model;
 import java.util.List;
 
 public interface Item {
+	String name();
+    Rarity rarity();
 	List<Coordonate> references();
 	default boolean rotate() {
 		return true;
