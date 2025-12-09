badd873b214eebdd38beee50cc2fb273116fa635
narykely <solofonarindraherinantenaina@gmail.com>
2025-12-09 14:50:16 +0100

    feat(StatsComponent) : création des méthodes pour générer des icônes et écriture des labels

--- BackTrack/src/fr/uge/view/StatsComponent.java
+++ BackTrack/src/fr/uge/view/StatsComponent.java
@@ -7,29 +7,53 @@ import java.util.Objects;
 
 import fr.uge.data.Hero;
 
-public record StatsComponent(int x, int y, Hero hero) implements Component{
+public record StatsComponent(int marginLeft, int marginTop, Hero hero) implements Component{
 	public StatsComponent {
 		Objects.requireNonNull(hero);
 	}
 	
+	private void drawLevelIcon(Graphics2D g, int x, int y) {
+		g.setColor(Color.YELLOW); 
+		g.setFont(new Font("Arial", Font.BOLD, 16)); g.drawString("⭐  ", x , y); 
+	}
+	
+	private void drawHeartIcon(Graphics2D g, int x, int y) {
+		g.setColor(Color.RED); 
+		g.setFont(new Font("Arial", Font.BOLD, 16)); g.drawString("❤️  ", x , y); 
+	}
+	private void drawKeyIcon(Graphics2D g, int x, int y) {
+		g.setColor(Color.BLACK); 
+		g.setFont(new Font("Arial", Font.BOLD, 16)); g.drawString("🗝️  ", x , y); 
+	}
+	private void drawEnergyIcon(Graphics2D g, int x, int y) {
+		g.setColor(Color.YELLOW); 
+		g.setFont(new Font("Arial", Font.BOLD, 16)); g.drawString("⚡  ", x , y); 
+	}
+	
 	@Override
-	public void draw(Graphics2D g) {
-		// Cadre
-		g.setColor(Color.WHITE);
-		g.setFont(new Font("Arial", Font.BOLD, 14));
-		
-		var offsetX = 0; // décalage x 
-		var offsetY = 0; // décalage Y
+	public void draw(Graphics2D g, Component component) {
+		var offsetX = 15; // décalage x 
+		var offsetY = 20; // décalage Y
+		var i = 20;
 		// offsetX ou offsetY s'incrémentent , on va voir à l'implémentation
-		g.drawString("⭐ Niveau     : " + hero.getLevel(), x + offsetX, y + offsetY);
-		g.drawString("❤️ Vie 				: " + hero.getHealthPoint(), x + offsetX, y + offsetY);
-		g.drawString("🔑 Clé        : " + hero.getKeys(), x + offsetX, y + offsetY);
-		g.drawString("⚡ Energy	    : " + hero.getEnergy(), x + offsetX, y + offsetY);
+		drawLevelIcon(g, marginLeft + offsetX, marginTop + offsetY);offsetX+=15;
+		g.setColor(Color.WHITE); g.setFont(new Font("Arial", Font.BOLD, 14)); g.drawString("Niveau     : " + hero.getLevel(), marginLeft +offsetX, marginTop + offsetY); offsetY += i;
 		
+		offsetX = 15; // retour à la position x initial
+		drawHeartIcon(g, marginLeft + offsetX, marginTop + offsetY);offsetX+=15;
+		g.setColor(Color.WHITE); g.setFont(new Font("Arial", Font.BOLD, 14)); g.drawString("Vie           : " + hero.getHealthPoint(), marginLeft + offsetX, marginTop + offsetY);offsetY += i;
+		
+		offsetX = 15; // retour à la position x initial
+		drawKeyIcon(g, marginLeft + offsetX, marginTop + offsetY);offsetX+=15;
+		g.setColor(Color.WHITE); g.setFont(new Font("Arial", Font.BOLD, 14)); g.drawString("Clé           : " + hero.getKeys(), marginLeft + offsetX, marginTop + offsetY);offsetY += i;
+		
+		offsetX = 15; // retour à la position x initial
+		drawEnergyIcon(g, marginLeft + offsetX, marginTop + offsetY);offsetX+=15;
+		g.setColor(Color.WHITE) ; g.setFont(new Font("Arial", Font.BOLD, 14)); g.drawString("Energy     : " + hero.getEnergy(), marginLeft + offsetX, marginTop + offsetY);	
 	}
 	
 	 @Override
    public int[] getBounds() {
-       return new int[]{x, y - 15, 500, 20};
+       return new int[]{marginLeft, marginTop - 15, 500, 20};
    }
 }
