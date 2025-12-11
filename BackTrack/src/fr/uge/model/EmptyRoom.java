daf47e481fe7ccabfbe7758a808fec561842612e
narykely <solofonarindraherinantenaina@gmail.com>
2025-12-11 22:43:58 +0100

    chore(EmptyRoom) : changement de record en class

--- BackTrack/src/fr/uge/model/EmptyRoom.java
+++ BackTrack/src/fr/uge/model/EmptyRoom.java
@@ -1,14 +1,31 @@
 package fr.uge.model;
 
-public record EmptyRoom(boolean isVisited) implements Room{
-
+public final class EmptyRoom implements Room{
+  
+	private final boolean isAccessible;
+	private final boolean isVisited;
+	
+	public EmptyRoom () {
+		this.isAccessible = true;
+		this.isVisited = false;
+	}
+	public EmptyRoom (boolean isAccessible, boolean isVisited) {
+		this.isAccessible = isAccessible;
+		this.isVisited = isVisited;
+	}
+	
+	@Override
+	public boolean isVisited() {
+		return isVisited;
+	}
+	
 	@Override	
 	public boolean isAccessible() {
 		return true;
 	};
 	    
 	public Room setVisited() {
-		return new EmptyRoom(true);
+		return new EmptyRoom(this.isAccessible, true);
 	}
 
 	@Override
