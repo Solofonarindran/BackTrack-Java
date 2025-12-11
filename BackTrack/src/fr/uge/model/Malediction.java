3cf868d0615aa90f66f3837eeb6ac8cd82e60b01
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-11 11:27:15 +0100

    feat(Malediction): ajout d'une méthode de création de malédiction

--- BackTrack/src/fr/uge/model/Malediction.java
+++ BackTrack/src/fr/uge/model/Malediction.java
@@ -2,15 +2,21 @@ package fr.uge.model;
 
 import java.util.List;
 
-public record Malediction(String name, Rarity rarity, List<Coordonate> size,List<Coordonate> references) implements Item{
+public record Malediction(String name, Rarity rarity, List<Coordonate> references) implements Item{
 	
 	public Malediction {
-		if( references.isEmpty()) {
+		if(references.isEmpty()) {
 			throw new IllegalArgumentException();
 		}
 	}
 	@Override
 	public boolean rotate() {
 		return false;
+	}
+	
+	public static Malediction initMalediction() {
+		 List<Coordonate> refs = List.of(new Coordonate(0, 1), new Coordonate(1, 1), new Coordonate(1, 0), new Coordonate(2, 0));
+	     var malediction = new Malediction("Malediction", Rarity.COMMON, refs);
+	     return malediction;
 	}
 }
