306eb34bf91ac5ae0c1cfa0cd38ccc1a5b41dbe7
Kalo5824 <kaloina.tissot@gmail.com>
2025-12-12 14:51:18 +0100

    chore(Fight): ajustement a java 25

--- BackTrack/src/fr/uge/rooms/Fight.java
+++ BackTrack/src/fr/uge/rooms/Fight.java
@@ -1,7 +1,15 @@
-package fr.uge.model;
+package fr.uge.rooms;
 
-import fr.uge.data.Hero;
-import fr.uge.data.Enemy;
+import fr.uge.item.FightItem;
+import fr.uge.item.Gold;
+import fr.uge.item.Item;
+import fr.uge.item.ItemFactory;
+import fr.uge.item.Malediction;
+import fr.uge.item.ManaStone;
+import fr.uge.actors.Enemy;
+import fr.uge.actors.EnemyAction;
+import fr.uge.actors.Hero;
+import fr.uge.backpack.Coordonate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,13 +66,13 @@ public class Fight {
 	private Enemy heroChoice(char action, Enemy enemy, Scanner scanner) {
 		Objects.requireNonNull(enemy);
 		switch(action) {
-		case 'A' -> System.out.println("Vous avez choisi d'attaquer ! Quelle arme souhaitez vous utiliser ?\n");
-		case 'P' -> System.out.println("Vous avez choisi de vous protéger ! Quelle protection souhaitez vous utiliser ?\n");
+		case 'A' -> IO.println("Vous avez choisi d'attaquer ! Quelle arme souhaitez vous utiliser ?\n");
+		case 'P' -> IO.println("Vous avez choisi de vous protéger ! Quelle protection souhaitez vous utiliser ?\n");
 		default -> throw new IllegalArgumentException("Erreur : le choix n'est pas valide");
 		};
 		
 		List<Item> itemInBackPack = hero.getBackPack().getItem();
-		System.out.println(hero.getBackPack());
+		IO.println(hero.getBackPack());
 		
 		int choice = scanner.nextInt();
 		int index = choice - 1;
@@ -82,9 +90,9 @@ public class Fight {
 		// On paie l'objet de combat
 		costWeapon(fightItem);
 		
-		System.out.println("Vous avez payé : " + fightItem.cost());
+		IO.println("Vous avez payé : " + fightItem.cost());
 				
-	    System.out.println("Vous utilisez : " + fightItem.name());
+	    IO.println("Vous utilisez : " + fightItem.name());
 	    
 	    // Appliquer l'effet selon le choix
 	    switch(action) {
@@ -103,8 +111,8 @@ public class Fight {
 		var ptsDegats = random.nextInt(max - min + 1) + min;
 		var protection = random.nextInt(max - min + 1) + min;
 		switch(choice) {
-		case 'A' -> System.out.println("L'ennemi a prévu d'attaquer de " + ptsDegats + " de dégâts !");
-		case 'P' -> System.out.println("L'ennemi a prévu de se protéger de " + protection + " !");
+		case 'A' -> IO.println("L'ennemi a prévu d'attaquer de " + ptsDegats + " de dégâts !");
+		case 'P' -> IO.println("L'ennemi a prévu de se protéger de " + protection + " !");
 		default -> throw new IllegalArgumentException("Erreur : le choix n'est pas valide");
 		};
 		return new EnemyAction(choice, ptsDegats, protection);
@@ -115,7 +123,7 @@ public class Fight {
 		List<Item> itemsAvailable = ItemFactory.getCatalog();
 		Random random = new Random();
 		var price = itemsAvailable.get(random.nextInt(itemsAvailable.size()));
-		System.out.println("Félicitations ! Vous avez gagné : " + price + " !");
+		IO.println("Félicitations ! Vous avez gagné : " + price + " !");
 		return price;
 	}
 	
@@ -125,17 +133,17 @@ public class Fight {
 			throw new IllegalArgumentException("Le nombre de refus ne peut pas être négatif !");
 		}
 		Malediction mal = Malediction.initMalediction();
-		System.out.println("L'ennemi vous a infligé une malédiction... L'acceptez vous ? (y/n)");
+		IO.println("L'ennemi vous a infligé une malédiction... L'acceptez vous ? (y/n)");
 		var choice = scanner.next().toLowerCase();
 		switch(choice) {
-		case "y" -> { System.out.println("Vous devez placer la malédiction dans votre BackPack.");
+		case "y" -> { IO.println("Vous devez placer la malédiction dans votre BackPack.");
 						/*
 						 * A INIT AVEC LES CLICKS
 						 */
 					  Coordonate clickedCoord;
 					  hero.getBackPack().addEquipement(mal, clickedCoord); 
 		}
-		case "n" -> { System.out.println("Attention, un kème refus vous vaudra k points de dégâts !");
+		case "n" -> { IO.println("Attention, un kème refus vous vaudra k points de dégâts !");
 					  nbRefus++;
 					  hero.takeDamage(nbRefus);
 		}
@@ -146,11 +154,11 @@ public class Fight {
 	
 	// On veut se débarasser d'un item
 	public void getRidOf(Item item, Scanner scanner) {
-		System.out.println("Voulez vous vraiment vous débarraser de " + item + " ? (y/n)");
+		IO.println("Voulez vous vraiment vous débarraser de " + item + " ? (y/n)");
 		var choice = scanner.next().toLowerCase();
 		switch(choice) {
 		case "y" : hero.getBackPack().removeEquipment(item);
-		case "n" : System.out.println("Action annulée.");
+		case "n" : IO.println("Action annulée.");
 		default : throw new IllegalArgumentException("L'entrée saisie n'est pas valide.");
 		}
 	}
@@ -158,20 +166,20 @@ public class Fight {
 	// ennemi mort -> partie gagnée
 	public boolean winGame(Enemy enemy, Scanner scanner) {
 		if(enemy.getHealthPoints() <= 0) {
-			System.out.println("Partie gagnée !\n");
+			IO.println("Partie gagnée !\n");
 			Item price = price();
 			/*
 			 * A INIT AVEC LES CLICKS
 			 */
 			Coordonate clickedCoord;
 			if(hero.getBackPack().addEquipement(price, clickedCoord)){
-				System.out.println("Vous avez placé : "  + price);
+				IO.println("Vous avez placé : "  + price);
 			} else {
-				System.out.println(price.name() + " n'a pas pu être placé... Désirez vous vous débarasser d'un autre objet ? (y/n)");
+				IO.println(price.name() + " n'a pas pu être placé... Désirez vous vous débarasser d'un autre objet ? (y/n)");
 				var decision = scanner.next().toLowerCase();
 				switch(decision) {
 				case "y" : getRidOf(price, scanner);
-				case "n" : System.out.println("Le nouvel item est alors laissé...");
+				case "n" : IO.println("Le nouvel item est alors laissé...");
 				}
 			}
 			return true;
@@ -180,10 +188,16 @@ public class Fight {
 	}
 	
 	// hero mort
-	public boolean lostGame(Scanner scanner) {
+	public boolean lostGame(Enemy enemy, Scanner scanner) {
 		if(hero.getHealthPoints() <= 0) {
-			System.out.println("Partie perdue !\n");
+			IO.println("Partie perdue !\n");
 			nbRefus = malediction(scanner);
+			hero.gainExperience(enemy.getNbXp());
+			/*
+			 * A INIT AVEC LES CLICKS
+			 */
+			List<Coordonate> clickedCoord;
+			hero.getBackPack().extendBackPack(enemy.getNbCases(), clickedCoord);
 			return true;
 		}
 		return false;
@@ -199,31 +213,31 @@ public class Fight {
 	public void fight() {
 		// à faire -> recup les anciens niveaux de vie, protection et équipements depuis la sauvegarde
 		Enemy enemy = enemyToFight(enemies);
-		System.out.println("Vous allez combattre : " + enemy.getName());
+		IO.println("Vous allez combattre : " + enemy.getName());
 		Scanner scanner = new Scanner(System.in);
-		while(!winGame(enemy, scanner) && !lostGame(scanner)) {
+		while(!winGame(enemy, scanner) && !lostGame(enemy, scanner)) {
 			// prévision enemi
 			var action = enemyChoice();
 			// choix hero
-			System.out.println("Héro, c'est à votre tour de jouer ! Quelle action voulez vous effectuer : attaquer(A) ou augmenter votre niveau de protection(P) ?\n");
+			IO.println("Héro, c'est à votre tour de jouer ! Quelle action voulez vous effectuer : attaquer(A) ou augmenter votre niveau de protection(P) ?\n");
 			char playerChoice = scanner.next().toUpperCase().charAt(0);
 			// action hero
 			enemy = heroChoice(playerChoice, enemy, scanner);
 			// action ennemi
 			switch(action.choice()) {
-			case 'A' -> { System.out.println("L'ennemi vous a infligé " + action.ptsDegats() + " points de dégâts !");
+			case 'A' -> { IO.println("L'ennemi vous a infligé " + action.ptsDegats() + " points de dégâts !");
 			hero = hero.takeDamage(action.ptsDegats());
 			}
-			case 'P' -> { System.out.println("L'ennemi a augmenté son niveau de protection de " + action.protection() + " points !"); 
+			case 'P' -> { IO.println("L'ennemi a augmenté son niveau de protection de " + action.protection() + " points !"); 
 			enemy = enemy.addProtection(action.protection());
 			}
 			};
 			// Pour tests 
-			System.out.println("Points de vie du hero : " + hero.getHealthPoints());
-			System.out.println("Points de vie de l'ennemi : " + enemy.getHealthPoints());
-			System.out.println("Points de protection du hero : " + hero.getProtection());
-			System.out.println("Points de protection de l'ennemi : " + enemy.getProtection());
-			System.out.println("Points d'énergie du hero : " + hero.getEnergy());
+			IO.println("Points de vie du hero : " + hero.getHealthPoints());
+			IO.println("Points de vie de l'ennemi : " + enemy.getHealthPoints());
+			IO.println("Points de protection du hero : " + hero.getProtection());
+			IO.println("Points de protection de l'ennemi : " + enemy.getProtection());
+			IO.println("Points d'énergie du hero : " + hero.getEnergy());
 		}
 		scanner.close();
 	}
