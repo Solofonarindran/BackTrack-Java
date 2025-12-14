1ca2728da40301c7d6b82ce4771410bc7f2837f2
narykely <solofonarindraherinantenaina@gmail.com>
2025-12-15 00:49:13 +0100

    chore(Room) : aucun

---
+++ BackTrack/src/fr/uge/data/Room.java
@@ -0,0 +1,30 @@
+package fr.uge.data;
+
+
+
+/**
+ * Interface représentant une salle du donjon.
+ * Chaque type de salle implémente cette interface.
+ */
+public sealed interface Room permits EmptyRoom, TreasureRoom, EnemyRoom, 
+                                      MerchantRoom, HealerRoom, ExitRoom, 
+                                       GateRoom, SurpriseRoom , BossRoom{
+    
+
+    /**
+     * @return true si le joueur peut traverser cette salle
+     */
+    boolean isAccessible();
+    
+    /**
+     * @return true si la salle a été visitée
+     */
+    boolean isVisited();
+    
+    /**
+     * Marque la salle comme visitée
+     */
+    Room setVisited();
+    
+    String getDescription();
+}
