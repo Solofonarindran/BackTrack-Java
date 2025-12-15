dc922f94bbb353683052232ed2a16cedc27b66f4
narykely <solofonarindraherinantenaina@gmail.com>
2025-12-15 01:25:21 +0100

    feat(Run) : test

--- BackTrack/src/fr/uge/controller/Run.java
+++ BackTrack/src/fr/uge/controller/Run.java
@@ -6,31 +6,34 @@ import com.github.forax.zen.Application;
 import com.github.forax.zen.ApplicationContext;
 
 import fr.uge.data.BackPack;
-import fr.uge.data.Hero;
+import fr.uge.model.Enemy;
+import fr.uge.model.Hero;
 import fr.uge.view.Index;
 
 public class Run {
-	public static void run(ApplicationContext context, Hero hero) {
+	public static void run(ApplicationContext context, Hero hero, Enemy enemy) {
 		
-		context.renderFrame(graphics -> Index.page().body(context, graphics, hero));
+		context.renderFrame(graphics -> Index.page().body(context, graphics, hero, enemy));
 	}
 	
-	public static void main(String[] args) {
-	// Initialisation
-	  var HEALTH_POINT = 10;
-	  var MAX_HEALTH_POINT = 30;
-	  var level = 1;
-	  var experience = 100;
-	  var maxEnergy = 3;
-	  var energy = 3;
-	  var manaPoint = 5;
-	  var protection = 10;
-	  var key = 1;
-	  
-	  var backpack = new BackPack();
-    var hero = new Hero(HEALTH_POINT,MAX_HEALTH_POINT, level, experience,maxEnergy, energy,manaPoint,protection, backpack,key);
-		Application.run(Color.BLACK, context -> {
-				  run(context,hero);
-		});
-	}
+//	public static void main(String[] args) {
+//	// Initialisation
+//	  var HEALTH_POINT = 20;
+//	  var MAX_HEALTH_POINT = 30;
+//	  var level = 1;
+//	  var experience = 100;
+//	  var maxEnergy = 3;
+//	  var energy = 3;
+//	  var manaPoint = 5;
+//	  var protection = 10;
+//	  var key = 1;
+//	  
+//	  var backpack = new BackPack();
+//    var hero = new Hero(HEALTH_POINT,MAX_HEALTH_POINT, level, experience,maxEnergy, energy,manaPoint,protection, backpack,key);
+//    var enemy = new Enemy(HEALTH_POINT - 15,MAX_HEALTH_POINT, level, experience,maxEnergy, energy,manaPoint,protection, backpack,key);
+//    
+//		Application.run(Color.BLACK, context -> {
+//				  run(context,hero,enemy);
+//		});
+//	}
 }
